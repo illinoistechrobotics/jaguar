34fa88a0508b5737c95ebf22744bed4333b6673f
Peter Chinetti <peter@chinetti.me>
2013-05-11 17:14:54 -0500

    added wrapper.c

---
+++ src/wrapper.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#define SERIALDEV "/dev/ttyACM0"
+int serial_file;
+int serial_init=0;
+int init(char * device){
+   serial_init=0;
+   struct termios spconfig;
+   int fd = open(device, O_RDWR | O_NOCTTY);
+   if(fd == -1 || !isatty(fd) || tcgetattr(fd, &spconfig) < 0) {
+      return -1;
+   }
+   cfmakeraw(&spconfig);
+   cfsetispeed(&spconfig, B115200);
+   cfsetospeed(&spconfig, B115200);
+   tcsetattr(fd,TCSANOW,&spconfig);
+   serial_init =1;
+   return serial_file = fd;
+}
+int main(int argc, char** argv){
+   printf("%d\n",init("/dev/ttyACM0"));
+   return 0;
+}
